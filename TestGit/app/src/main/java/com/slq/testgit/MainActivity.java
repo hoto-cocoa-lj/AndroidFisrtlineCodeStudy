b6d87003bf2738039db28b7b05c9e7a330fe8bc2
Ironi333 <657578087@qq.com>
2020-12-19 16:25:44 +0800

    123123

--- TestGit/app/src/main/java/com/slq/testgit/MainActivity.java
+++ TestGit/app/src/main/java/com/slq/testgit/MainActivity.java
@@ -17,13 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+123123123123123
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
