7d8ce91fab98555584663ba82737dea7ce82e1cb
Ironi333 <657578087@qq.com>
2020-12-19 16:27:58 +0800

    clone commits

--- TestGit/app/src/main/java/com/slq/testgit/MainActivity.java
+++ TestGit/app/src/main/java/com/slq/testgit/MainActivity.java
@@ -17,14 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-123
-        123123123123123
-        @Override
-        public void onClick(View view) {
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-        }
-    });
     }
 
     @Override
